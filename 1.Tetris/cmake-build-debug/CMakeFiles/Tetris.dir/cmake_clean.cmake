aa23bfbadaa65f6dca7fce7f616457492f8243fc
fangxlmr <fangxlmr@foxmail.com>
2018-05-21 05:07:51 +0800

    new project: tetris

---
+++ 1.Tetris/cmake-build-debug/CMakeFiles/Tetris.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tetris.dir/demo.c.obj"
+  "Tetris.pdb"
+  "Tetris.exe"
+  "Tetris.exe.manifest"
+  "libTetris.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Tetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
