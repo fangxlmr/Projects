595a2c58162a4d2c8c0e89b156500c28aca7f59f
fangxlmr <fangxlmr@foxmail.com>
2018-06-21 20:43:36 +0800

    store words in bstree and inorder traversal of bstree

--- word_frequency/wf.c
+++ word_frequency/wf.c
@@ -5,8 +5,33 @@
 #include <stdlib.h>
 #include "bstree.h"
 #define MAXLINE 5000    /* 行内最大字数 */
+#define TOP 10
 
+BSTree_T ary[TOP];  /* BSTree_T array of size n (n = TOP) */
+int min;    /* min of count in BSTree_T array */
+int index;  /* index of min */
+int empty;  /* empty position */
+
+void to_lower(char *line);
 char *getword(char *str);
+void gettop(BSTree_T root);
+
+/* lower the case of chars in the line */
+void to_lower(char *s)
+{
+    int i;
+    
+    if (!s) {
+        return;
+    }
+    i = 0;
+    while (s[i] != '\0') {
+        if (isupper(s[i]) != 0) {
+            s[i] = tolower(s[i]);
+        }
+        ++i;
+    }
+}
 
 /* get a word from a string */
 char *getword(char *s)
@@ -39,6 +64,26 @@ char *getword(char *s)
     return index;
 }
 
+/*
+ * Get top n of count
+ */
+/*void gettop(BSTree_T root)
+{
+    if (!root) {
+        return;
+    }
+    gettop(root->left);
+
+    if (empty < TOP) {
+        ary[empty] = root;
+        if (root->count < min) {
+
+        }
+    }
+
+    gettop(root->right);
+}*/
+
 int main(void)
 {
     char line[MAXLINE];
@@ -49,9 +94,10 @@ int main(void)
     root = bst_new();
     fp = fopen("./Knowledge and Virtue.txt", "r");
     while (fgets(line, MAXLINE, fp)) {
+        to_lower(line);
         word = getword(line);
+        printf("%s\n", line);
         while (word != NULL) {
-//            printf("%s\n", word);
             bst_insert(&root, word);
             word = getword(NULL);
         }
